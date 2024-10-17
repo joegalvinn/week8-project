046977dad9c2d1a384b53a616495abda616794c0
Joseph <joseph.galvin@digisolve.co.uk>
2024-10-17 16:09:04 +0100

    skeleton

---
+++ project_file/src/app/newPost/page.js
@@ -0,0 +1,10 @@
+//navigaton
+//metadata
+
+export default function NewPostPage() {
+  //here i need to handle the submit of the post form
+  return (
+    <>A form to add a new blog post</>
+    //here i need a form to collect data from the user
+  );
+}
